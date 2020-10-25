2460dea43d941238d7006565adcad3a0878f1999
German Novikov <gnovikov@nextpage.agency>
2020-10-25 10:05:51 +0200

    initial commit

---
+++ src/components/header/header.js
@@ -0,0 +1,31 @@
+import "./index.scss";
+import bgLogo from "./images/star-top.svg";
+// import topPhoto from "./images/top-foto.jpg"
+import rugbyLogo from "./images/rugby_europe_logo.png"
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header" style={{backgroundImage: `url(${bgLogo})`}}>
+      <div className="content-block">
+        <div className="title-block">
+          <img src={rugbyLogo} alt=""/>
+          <div>
+            <h1>Kirill Yashenkov</h1>
+            <p>
+              Candidate’s Program for the post of President of Rugby Europe
+            </p>
+          </div>
+          
+        </div>
+      </div>
+     
+      
+      {/*<img src={topPhoto} alt=""/>*/}
+    </div>
+  );
+}
+
+
+
+export default Header;
