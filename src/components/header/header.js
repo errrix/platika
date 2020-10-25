b6fc2e5c35a5f05e307ff49abda4b5e21eadce45
Yevhen Ivanov <yevhen.ivanov@ab-soft.net>
2020-10-25 23:59:07 +0200

    header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,35 +1,86 @@
 import "./index.scss";
 import bgLogo from "./images/star-top.svg";
-import { withTranslation } from 'react-i18next';
-// import topPhoto from "./images/top-foto.jpg"
-import rugbyLogo from "./images/rugby_europe_logo.png"
+import {withTranslation} from 'react-i18next';
+import topPhoto from "./images/top-foto.png";
+import headerFrame from "./images/header-frame.png";
+import rugbyLogo from "./images/rugby_europe_logo.png";
+import yashenkovPhoto from "./images/yashenkov.png";
 import React from "react";
 
-function Header({ t, i18n }) {
+function Header({t, i18n}) {
     return (
-      <div className="header" style={{backgroundImage: `url(${bgLogo})`}}>
-        <div className="content-block">
-          <div className="title-block">
-            <img src={rugbyLogo} alt=""/>
-            <div>
-              <h1>{t("header.name")}</h1>
-              <p>
-                {t("header.intro")}
-              </p>
+        <div className="header" style={{backgroundImage: `url(${bgLogo})`}}>
+            <div className="header__content-block">
+                <div className="header__title-block">
+                    <div className="header__text-image">
+                        <div className="header__rugby-logo">
+                            <img src={rugbyLogo} alt=""/>
+                        </div>
+                        <div>
+                            <h1 className="header__title">{t("header.name")}</h1>
+                            <h2 className="header__subtitle">
+                                {t("header.intro")}
+                            </h2>
+                        </div>
+                    </div>
+                    <div className="header__button-block">
+                        <button onClick={() => i18n.changeLanguage('en')}>En</button>
+                        <button onClick={() => i18n.changeLanguage('fr')}>Fr</button>
+                        <button onClick={() => i18n.changeLanguage('ru')}>Ru</button>
+                        <button onClick={() => i18n.changeLanguage('de')}>De</button>
+                        <button onClick={() => i18n.changeLanguage('es')}>Es</button>
+                        <button onClick={() => i18n.changeLanguage('it')}>It</button>
+                    </div>
+
+                    <div className="header__menu">
+                        <div className="header__menu--column-1">
+                            <a href="#introduction">{t("header.link1")}</a>
+                            <a href="#introduction">{t("header.link2")}</a>
+                            <a href="#introduction">{t("header.link3")}</a>
+                            <a href="#introduction">{t("header.link4")}</a>
+                            <a href="#introduction">{t("header.link5")}</a>
+                            <a href="#introduction">{t("header.link6")}</a>
+                            <a href="#introduction">{t("header.link7")}</a>
+                            <a href="#introduction">{t("header.link8")}</a>
+                        </div>
+                        <div className="header__menu--column-2">
+                            <a href="#introduction">{t("header.link9")}</a>
+                            <a href="#introduction">{t("header.link10")}</a>
+                            <a href="#introduction">{t("header.link11")}</a>
+                            <a href="#introduction">{t("header.link12")}</a>
+                            <a href="#introduction">{t("header.link13")}</a>
+                            <a href="#introduction">{t("header.link14")}</a>
+                            <a href="#introduction">{t("header.link15")}</a>
+                            <a href="#introduction">{t("header.link16")}</a>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="header__image-block">
+                    <img src={topPhoto} alt=""/>
+                </div>
             </div>
-        
-          </div>
+
+            <div className="header__slanting-line">
+                <img src={headerFrame}/>
+            </div>
+
+            <div className="header__about" style={{backgroundImage: `url(${yashenkovPhoto})`}}>
+                <a name="introduction"></a>
+                <p className="header__about-text" >
+                    {t("header.about")}
+                    <p className="header__about-strong">
+                        {t("header.about-strong")}
+                    </p>
+                    <p className="header__about-signature">
+                        {t("header.about-signature")}
+                    </p>
+                </p>
+            </div>
         </div>
-        <div className="button-block">
-          <button onClick={() => i18n.changeLanguage('en')}>EN</button>
-          <button onClick={() => i18n.changeLanguage('ru')}>RU</button>
-        </div>
-      
-        {/*<img src={topPhoto} alt=""/>*/}
-      </div>
     );
 }
 
 
-
-export default  withTranslation("common")(Header);
+export default withTranslation("common")(Header);
