f63e3ef89293c85ca6a9b18557cd42493c929778
Yevhen Ivanov <yevhen.ivanov@ab-soft.net>
2020-10-27 22:42:26 +0200

    mob

--- src/components/audience/audience.js
+++ src/components/audience/audience.js
@@ -2,16 +2,20 @@ import "./index.scss";
 import {withTranslation} from 'react-i18next';
 import React from "react";
 import bgAudience from "../audience/images/audience-bg.png";
+import audienceMob from "../audience/images/audience-mob.png";
 
 function Audience({t, i18n}) {
     return (
         <div className="audience" style={{backgroundImage: `url(${bgAudience})`}}>
             <a name="audience"></a>
             <div className="audience__content-block">
+                <div className="audience__image-block">
+                    <img className="audience__image" src={audienceMob} alt="Audience is the main capital"/>
+                </div>
                 <div className="audience__info">
-                   <div className="audience__title">
-                       {t("audience.title")}
-                   </div>
+                    <div className="audience__title">
+                        {t("audience.title")}
+                    </div>
                     <div className="audience__text">
                         <span>
                             {t("audience.text1")}
--- src/components/electronic-data/electronic-data.js
+++ src/components/electronic-data/electronic-data.js
@@ -2,6 +2,7 @@ import "./index.scss";
 import {withTranslation} from 'react-i18next';
 import React from "react";
 import bgElData from "../electronic-data/images/electronic-data-bg.png";
+import macMob from "../electronic-data/images/mac_mob.png";
 
 function ElectronicData({t, i18n}) {
     return (
@@ -9,6 +10,9 @@ function ElectronicData({t, i18n}) {
             <a name="electronic-data"></a>
             <div className="electronic-data__content-block">
                 <div className="electronic-data__info">
+                    <div className="electronic-data__image-block">
+                        <img className="electronic-data__image" src={macMob} alt="WORLD RUGBY AND THE INTERCONTINENTAL PARTNERSHIP"/>
+                    </div>
                    <div className="electronic-data__title">
                        {t("electronic-data.title")}
                    </div>
