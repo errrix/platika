830d45fa50062612392d8dfd52bb540151ab2936
German Novikov <gnovikov@nextpage.agency>
2020-10-28 16:17:46 +0200

    add video

--- src/components/video/video.js
+++ src/components/video/video.js
@@ -1,20 +1,43 @@
 import "./index.scss";
-import {withTranslation} from 'react-i18next';
-import React from "react";
+import React, {Component} from "react";
 import bgVl from "../video/images/video-line.png";
 import playVideo from "../video/images/video-img.png";
 
-function Video({t, i18n}) {
+class  Video extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showVideo: false
+    };
+  }
+  playVideoHandler = () => {
+    this.setState({
+      showVideo: true
+    });
+  }
+  
+  render() {
     return (
-        <div className="video" style={{backgroundImage: `url(${bgVl})`}}>
-            <div className="video__content-block">
-                <div className="video__play-video">
-                    <img className="video__play-video--image" src={playVideo} alt="play"/>
-                </div>
-            </div>
+      <div className="video" style={{backgroundImage: `url(${bgVl})`}}>
+        <div className="video__content-block">
+          <div className="video__play-video">
+            {!this.state.showVideo &&
+              <img className="video__play-video--image"
+                src={playVideo}
+                onClick={this.playVideoHandler}
+                alt="play"/>
+            }
+            {this.state.showVideo &&
+            <iframe className="iframe" width="630" height="400" src="https://www.youtube.com/embed/IdwLbtYlkSA?&autoplay=1" frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen/>
+            }
+          </div>
         </div>
+      </div>
     );
+  }
 }
 
 
-export default withTranslation("common")(Video);
+export default Video;
